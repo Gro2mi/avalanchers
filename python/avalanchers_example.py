4a98869cf4843485e2205d68375d822a58640d69
Markus Rampp <dev@markusrampp.eu>
2026-04-20 21:23:38 +0200

    feat(wasm): move ava data into frontend for web server

--- python/avalanchers_example.py
+++ python/avalanchers_example.py
@@ -1,12 +1,12 @@
 import avalanchers
 
 settings = {
-    "dem_path": "data/avaframe/avaMal.png",
+    "dem_path": "frontend/data/avaframe/avaKot.png",
 }
 sim = avalanchers.PySimulation.create(settings)
 
 # or easier for default settings
-# sim = avalanchers.PySimulation.create_default("data/avaframe/avaMal.png")
+# sim = avalanchers.PySimulation.create_default("frontend/data/avaframe/avaMal.png")
 
 sim.run()
 avalanchers.plot3d(sim, "max_velocity")
